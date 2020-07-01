20195d704e71dea74368760184dcce23c749506c
kimkim0814 <tnryj505@gmail.com>
2020-07-01 16:38:22 +0900

    Update filters.py

--- app/filters.py
+++ app/filters.py
@@ -1,33 +1,33 @@
-# from django_filters　import filters
-# from django_filters import FilterSet
-# from .models import Item
+from django_filters import filters
+from django_filters import FilterSet
+from .models import Item
 
 
 
 
-# class MyOrderingFilter(filters.OrderingFilter):
-#     descending_fmt = '%s （降順）'
+class MyOrderingFilter(filters.OrderingFilter):
+    descending_fmt = '%s （降順）'
 
 
-# class ItemFilter(FilterSet):
+class ItemFilter(FilterSet):
 
-#     name = filters.CharFilter(label='氏名', lookup_expr='contains')
-#     memo = filters.CharFilter(label='備考', lookup_expr='contains')
+    name = filters.CharFilter(label='氏名', lookup_expr='contains')
+    memo = filters.CharFilter(label='備考', lookup_expr='contains')
 
-#   order_by = MyOrderingFilter(
-#         # tuple-mapping retains order
-#         fields=(
-#             ('name', 'name'),
-#             ('age', 'age'),
-#         ),
-#         field_labels={
-#             'name': '氏名',
-#             'age': '年齢',
-#         },
-#         label='並び順'
-#     )
+  order_by = MyOrderingFilter(
+        # tuple-mapping retains order
+        fields=(
+            ('name', 'name'),
+            ('age', 'age'),
+        ),
+        field_labels={
+            'name': '氏名',
+            'age': '年齢',
+        },
+        label='並び順'
+    )
 
-#     class Meta:
+    class Meta:
 
-#         model = Item
-#         fields = ('name', 'sex', 'memo',)
+        model = Item
+        fields = ('name', 'sex', 'memo',)
